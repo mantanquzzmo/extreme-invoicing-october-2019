81067a002e2e787165a4411b5883a3dc271c87a4
mantanquzzmo <quzzmo@hotmail.com>
2019-12-22 14:42:04 +0100

    adds javascript linkage

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,11 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+document.addEventListener('turbolinks:load', () => {
+  let stripeForm = document.getElementById('payment_form')
+
+  if (stripeForm) {
+
+  }
+})
